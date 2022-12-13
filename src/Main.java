f56ad6a432989014e31ac4e7996279dfe90cf8f7
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-13 09:23:46 +0100

    Commit init

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String string1 = "I Like to code near the Lake with my friend Luke";
+        String string2 = string1.replaceAll("L[^u]ke*","BLA");
+        System.out.println(string2);
+    }
+}
